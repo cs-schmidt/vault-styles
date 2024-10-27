7e2d887058da0a9edf39d9727b03634f1c8e16ce
cs-schmidt <ebs.euler@gmail.com>
2024-10-27 15:24:37 -0600

    feat: enhance styles and refactor

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,4 +1,11 @@
 /** @typedef {import('stylelint').Config} StylelintConfig */
 
 /** @type {StylelintConfig} */
-export default { extends: ['stylelint-config-standard', 'stylelint-config-clean-order'] };
+export default {
+  extends: ['stylelint-config-standard', 'stylelint-config-clean-order'],
+  rules: {
+    'no-descending-specificity': [true, { ignore: ['selectors-within-list'] }],
+    'selector-class-pattern': null,
+    'selector-id-pattern': null,
+  },
+};
